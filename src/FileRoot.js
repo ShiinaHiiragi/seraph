b3214eedcb111624d90b3e42ad9ace65a51a1c4c
Shiina <shiinahiiragi@outlook.com>
2023-09-10 14:03:19 +0800

    delete third column

--- src/FileRoot.js
+++ src/FileRoot.js
@@ -80,7 +80,6 @@ export default function FileRoot() {
           gridTemplateColumns: {
             xs: "1fr",
             sm: "minmax(64px, 200px) minmax(450px, 1fr)",
-            md: "minmax(160px, 300px) minmax(600px, 1fr) minmax(300px, 420px)",
           },
           ...(drawerOpen && {
             height: "100vh",
