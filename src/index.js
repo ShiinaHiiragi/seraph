30f968a8f2af13767e5b9cd3286ede45c27900c3
Shiina <shiinahiiragi@outlook.com>
2023-09-10 13:37:44 +0800

    add templates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import FileRoot from "./Root";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "@fontsource/inter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<FileRoot />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
