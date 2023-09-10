70b630c2381b92273bfccaf67c6d00ce29323845
Shiina <shiinahiiragi@outlook.com>
2023-09-11 00:23:05 +0800

    add fonts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Panel from "./Panel";
 import reportWebVitals from "./reportWebVitals";
-import "@fontsource/inter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
