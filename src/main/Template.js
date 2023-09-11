032359bce8ad7487845dfddcb9838069f4841dd1
Shiina <shiinahiiragi@outlook.com>
2023-09-11 12:09:09 +0800

    change template

--- src/main/Template.js
+++ src/main/Template.js
@@ -1,15 +1,27 @@
-import RouteField from "../interface/RouteField";
+import React from "react";
 import { useParams } from "react-router";
+import RouteField from "../interface/RouteField";
+import GlobalContext from "../interface/constants";
 
 // TODO: change name of Template
-const Template = () => {
+const Template = (props) => {
+  const {
+    // TODO: fill parameters
+  } = props;
   const {
     // TODO: fill parameters
   } = useParams();
+  const context = React.useContext(GlobalContext);
 
   return (
-    <RouteField>
-      {/* TODO: fill contents */}
+    <RouteField
+      path={""}
+      title={""}
+      sx={{
+        overflowY: "hidden",
+        overflowX: "hidden"
+      }}
+    >
     </RouteField>
   )
 }
