0b00b5f6e018bd0d853c02ae9d9f66fdd153ae9d
Shiina <shiinahiiragi@outlook.com>
2023-10-14 17:39:01 +0800

    stash for picker

--- src/main/TODO.js
+++ src/main/TODO.js
@@ -1,5 +1,5 @@
 import React from "react";
-import RouteField from "../interface/RouteField";
+import Countdown from "react-countdown";
 import { styled } from "@mui/joy/styles";
 import List from "@mui/joy/List";
 import ListItem from "@mui/joy/ListItem";
@@ -13,10 +13,18 @@ import MenuButton from "@mui/joy/MenuButton";
 import MenuItem from "@mui/joy/MenuItem";
 import Dropdown from "@mui/joy/Dropdown";
 import IconButton from "@mui/joy/IconButton";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import Textarea from "@mui/joy/Textarea";
+import CircularProgress from "@mui/joy/CircularProgress";
 import MoreVertRoundedIcon from "@mui/icons-material/MoreVertRounded";
 import TodayOutlinedIcon from "@mui/icons-material/TodayOutlined";
 import AccessAlarmsOutlinedIcon from "@mui/icons-material/AccessAlarmsOutlined";
+import RouteField from "../interface/RouteField";
 import GlobalContext from "../interface/constants";
+import ModalForm from "../modal/Form";
+import Picker from "../components/Picker";
 
 const Item = styled(ListItem)(({ theme }) => ({
   display: "flex",
@@ -119,6 +127,35 @@ const TODO = () => {
         </Item>
         <ListDivider inset="startDecorator" />
       </List>
+      {/* <Countdown
+        date={Date.now() + 60000}
+        intervalDelay={0}
+        precision={2}
+        renderer={(props) => <CircularProgress size="sm" determinate value={props.total / 600} />}
+      /> */}
+      <ModalForm
+        open={true}
+        loading={false}
+        disabled={false}
+        handleClose={() => { }}
+        handleClick={() => { }}
+        title={"123"}
+        caption={"234"}
+        button={context.languagePicker("universal.button.submit")}
+      >
+        <FormControl>
+          <FormLabel>Task</FormLabel>
+          <Input placeholder="PLACEHOLDER" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Description</FormLabel>
+          <Textarea minRows={4} maxRows={4} placeholder="PLACEHOLDER" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Due</FormLabel>
+          <Picker />
+        </FormControl>
+      </ModalForm>
     </RouteField>
   )
 }
