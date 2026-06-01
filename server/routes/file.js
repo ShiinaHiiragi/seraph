0590ccaca80a718d78b56659e70f984800c85242
ichinoe <shiinahiiragi@outlook.com>
2026-06-01 13:50:58 +0800

    apply config to epub

--- server/routes/file.js
+++ server/routes/file.js
@@ -465,12 +465,33 @@ router.post('/epub', (req, res, next) => {
   }
 
   // use async function to avoid blocking
+  const config = api.configOperator.config.setting.epub;
   child.execFile(api.checkerOperator.python, [
     api.extentPath.epubConverterFilePath,
     '-c',
     JSON.stringify({
       "path.src": filePath,
-      "path.dst": folderPath
+      "path.dst": folderPath,
+      "page.split": config.page.split,
+      "page.front": config.page.front,
+      "nav.link": config.nav.link,
+      "nav.prev": config.nav.prev,
+      "nav.next": config.nav.next,
+      "fade.kana": config.fade.kana,
+      "fade.opaque": `0.${config.fade.opaque}`,
+      "fade.size": `0.${config.fade.size}em`,
+      "fade.top": `${config.fade.top}px`,
+      "image.show": config.image.show,
+      "image.width": config.image.width,
+      "image.ialt": config.image.altInline,
+      "image.balt": config.image.altBlock,
+      "image.spec": config.image.spec,
+      "page.clear": config.text.clearLine,
+      "ruby.show": config.text.showRuby,
+      "break.text": config.text.breakLine,
+      "out.html": config.out.html,
+      "out.keep": config.out.keep,
+      "out.vert": config.out.vert
     })
   ], { env: { ...process.env, PYTHONUTF8: '1' } }, (err, stdout, stderr) => {
     if (err) {
