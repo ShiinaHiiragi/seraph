8200b5a897ca54015c997e10565d10204e6422ad
Ichinoe <shiinahiiragi@outlook.com>
2026-06-08 23:52:49 +0800

    stage milkdown

--- src/main/Milkdown.js
+++ src/main/Milkdown.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { styled } from "@mui/joy/styles";
 import Box from "@mui/joy/Box";
 import { Crepe } from "@milkdown/crepe";
 import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
@@ -6,19 +7,31 @@ import RouteField from "../interface/RouteField";
 import GlobalContext from "../interface/constants";
 
 import "@milkdown/crepe/theme/common/style.css";
-import "@milkdown/crepe/theme/frame.css";
+import "@milkdown/crepe/theme/nord.css";
 
-const editorWrapStyle = {
+const MaildownField = styled(Box)(({ theme }) => ({
   flex: 1,
   display: "flex",
   flexDirection: "column",
   minHeight: 0,
-};
+  "& [data-milkdown-root]": {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    minHeight: 0
+  },
+  "& .milkdown": {
+    flex: 1,
+    overflowY: "auto",
+    minHeight: 0
+  }
+}));
 
 const CrepeEditorInner = () => {
-  useEditor((root) => {
-    return new Crepe({ root, defaultValue: "" });
-  }, []);
+  const { get } = useEditor((root) => {
+    return new Crepe({ root })
+  });
+
   return <Milkdown />;
 };
 
@@ -33,12 +46,13 @@ const CrepeEditor = () => {
         context.languagePicker("nav.utility.milkdown")
       ]}
       title={context.languagePicker("nav.utility.milkdown")}
+      sx={{ flexGrow: 1, minHeight: 0, height: "auto" }}
     >
-      <Box sx={editorWrapStyle}>
+      <MaildownField>
         <MilkdownProvider>
           <CrepeEditorInner />
         </MilkdownProvider>
-      </Box>
+      </MaildownField>
     </RouteField>
   );
 }
