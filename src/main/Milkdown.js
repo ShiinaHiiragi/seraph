8966dbf1b5cf0c69431651ec39fbfab01632bf75
ichinoe <shiinahiiragi@outlook.com>
2026-06-12 16:25:53 +0800

    add links

--- src/main/Milkdown.js
+++ src/main/Milkdown.js
@@ -6,6 +6,10 @@ import { Crepe, CrepeFeature } from "@milkdown/crepe";
 import { editorViewOptionsCtx } from "@milkdown/kit/core";
 import { getMarkdown, replaceAll } from "@milkdown/kit/utils";
 import { listener, listenerCtx } from "@milkdown/kit/plugin/listener";
+import { editorViewCtx } from "@milkdown/kit/core";
+import { linkTooltipAPI } from "@milkdown/kit/component/link-tooltip";
+import { keymap } from "@milkdown/kit/prose/keymap";
+import { $prose } from "@milkdown/kit/utils";
 // import { emoji } from "@milkdown/plugin-emoji";
 import Loading from "./Loading";
 import RouteField from "../interface/RouteField";
@@ -60,17 +64,16 @@ const CrepeEditorInner = (props) => {
         ? context.crepeRef.snapshot.current
         : fileContent,
       features: {
-        // [CrepeFeature.blockEdit]: root.offsetWidth >= 552,
-        [CrepeFeature.Toolbar]: false,
-        [CrepeFeature.TopBar]: true
+        [CrepeFeature.Toolbar]: true,
       },
       featureConfigs: {
         [CrepeFeature.Cursor]: {
           width: 2,
         },
-        [CrepeFeature.Placeholder]: {
-          text: context.languagePicker("nav.utility.milkdown")
-        },
+        [Crepe.Feature.LinkTooltip]: {
+          inputPlaceholder: "Enter URL...",
+          onCopyLink: () => console.log("Link copied"),
+        }
       },
     });
 
@@ -98,6 +101,17 @@ const CrepeEditorInner = (props) => {
           context.crepeRef.modify.current = true;
         });
       })
+      .use($prose((ctx) => keymap({
+        "Mod-k": () => {
+          const { selection } = ctx.get(editorViewCtx).state;
+          if (selection.empty) {
+            return false;
+          }
+          ctx.get(linkTooltipAPI.key)
+            .addLink(selection.from, selection.to);
+          return true;
+        }
+      })))
       .use(listener);
 
     context.crepeRef.load(crepe.editor, { getMarkdown, replaceAll });
@@ -106,6 +120,7 @@ const CrepeEditorInner = (props) => {
     context.setting.meta.language
     // TODO: add config in context.setting
     // spell check
+    // enable tool bar
   ]);
 
   React.useEffect(() => {
@@ -130,45 +145,7 @@ const CrepeEditor = (props) => {
       // TODO: get filename by url path
       // TODO: get text from server via filename
       setCrepeState(1);
-      setFileContent(`Aliqua reprehenderit minim ut amet ullamco magna deserunt do consectetur. Occaecat irure veniam duis laborum cupidatat ut ipsum aute. Labore deserunt laborum non et excepteur labore quis sint. Consectetur reprehenderit voluptate consequat ea non commodo proident quis voluptate fugiat non irure cillum.
-
-Aute quis voluptate consequat tempor aliqua. Ipsum occaecat aliquip duis consequat elit quis consectetur duis minim eiusmod duis labore voluptate laborum. Elit in sint qui cillum ipsum Lorem dolor qui. Do id do culpa reprehenderit qui. In exercitation amet sunt ad ex sit commodo tempor. Culpa veniam eu id culpa labore qui cupidatat mollit deserunt.
-      
-Incididunt sint voluptate officia magna laboris anim exercitation occaecat et nisi id. Eiusmod Lorem nostrud sint cupidatat aliquip. Do aute aliqua exercitation dolor culpa excepteur ut. Exercitation duis veniam officia id culpa irure tempor. Dolor fugiat mollit anim ex mollit irure. Veniam do amet et ex incididunt eiusmod incididunt qui eiusmod officia sunt voluptate ipsum cupidatat.
-      
-Ad aliqua eiusmod duis eu elit. Officia exercitation reprehenderit reprehenderit Lorem laboris id reprehenderit ex anim veniam dolore aute anim. Laborum eu do aute ea.
-      
-Velit sunt consectetur velit aliquip Lorem. Laborum exercitation adipisicing in cupidatat. Nostrud culpa et pariatur fugiat commodo ad. Irure aliquip ut ullamco ad minim nisi.
-      
-Reprehenderit pariatur esse dolore pariatur ex fugiat nisi esse nulla deserunt dolor. Commodo incididunt consequat velit enim. Velit elit magna et consequat magna cillum cupidatat. Cupidatat deserunt pariatur nostrud ipsum consectetur incididunt deserunt nisi pariatur. Irure veniam Lorem eiusmod et excepteur ad dolore dolore. Incididunt elit cupidatat quis est exercitation minim.
-      
-Cupidatat cillum ea in enim occaecat et laborum officia nostrud duis ut deserunt culpa et. Quis ea amet et id ipsum veniam reprehenderit consectetur id duis esse. Esse laborum laboris sit Lorem sit voluptate in in laborum ipsum Lorem. Eiusmod commodo nulla eu cillum laboris laboris mollit voluptate nulla. Enim voluptate aliquip sit excepteur est. Elit nulla aliqua mollit anim exercitation labore cupidatat officia fugiat elit laborum do.
-      
-Minim velit reprehenderit occaecat anim proident sunt in esse. Anim deserunt labore aliquip in amet labore ea sunt deserunt occaecat. Et dolore consequat tempor occaecat eiusmod cupidatat voluptate.
-      
-Commodo cillum officia duis cillum mollit sunt excepteur velit laboris reprehenderit id Lorem veniam consectetur. Laboris eiusmod aliquip dolore ex voluptate minim occaecat dolor est ut mollit dolore. Qui eiusmod anim duis cupidatat nostrud minim nostrud tempor amet est velit culpa non.
-      
-Laborum deserunt sit laborum Lorem sit in dolore velit aliquip. Non ex pariatur excepteur nisi ipsum reprehenderit amet laborum incididunt ad. Adipisicing tempor eiusmod enim fugiat sit velit ad quis ex reprehenderit deserunt.
-
-Aliqua reprehenderit minim ut amet ullamco magna deserunt do consectetur. Occaecat irure veniam duis laborum cupidatat ut ipsum aute. Labore deserunt laborum non et excepteur labore quis sint. Consectetur reprehenderit voluptate consequat ea non commodo proident quis voluptate fugiat non irure cillum.
-
-Aute quis voluptate consequat tempor aliqua. Ipsum occaecat aliquip duis consequat elit quis consectetur duis minim eiusmod duis labore voluptate laborum. Elit in sint qui cillum ipsum Lorem dolor qui. Do id do culpa reprehenderit qui. In exercitation amet sunt ad ex sit commodo tempor. Culpa veniam eu id culpa labore qui cupidatat mollit deserunt.
-      
-Incididunt sint voluptate officia magna laboris anim exercitation occaecat et nisi id. Eiusmod Lorem nostrud sint cupidatat aliquip. Do aute aliqua exercitation dolor culpa excepteur ut. Exercitation duis veniam officia id culpa irure tempor. Dolor fugiat mollit anim ex mollit irure. Veniam do amet et ex incididunt eiusmod incididunt qui eiusmod officia sunt voluptate ipsum cupidatat.
-      
-Ad aliqua eiusmod duis eu elit. Officia exercitation reprehenderit reprehenderit Lorem laboris id reprehenderit ex anim veniam dolore aute anim. Laborum eu do aute ea.
-      
-Velit sunt consectetur velit aliquip Lorem. Laborum exercitation adipisicing in cupidatat. Nostrud culpa et pariatur fugiat commodo ad. Irure aliquip ut ullamco ad minim nisi.
-      
-Reprehenderit pariatur esse dolore pariatur ex fugiat nisi esse nulla deserunt dolor. Commodo incididunt consequat velit enim. Velit elit magna et consequat magna cillum cupidatat. Cupidatat deserunt pariatur nostrud ipsum consectetur incididunt deserunt nisi pariatur. Irure veniam Lorem eiusmod et excepteur ad dolore dolore. Incididunt elit cupidatat quis est exercitation minim.
-      
-Cupidatat cillum ea in enim occaecat et laborum officia nostrud duis ut deserunt culpa et. Quis ea amet et id ipsum veniam reprehenderit consectetur id duis esse. Esse laborum laboris sit Lorem sit voluptate in in laborum ipsum Lorem. Eiusmod commodo nulla eu cillum laboris laboris mollit voluptate nulla. Enim voluptate aliquip sit excepteur est. Elit nulla aliqua mollit anim exercitation labore cupidatat officia fugiat elit laborum do.
-      
-Minim velit reprehenderit occaecat anim proident sunt in esse. Anim deserunt labore aliquip in amet labore ea sunt deserunt occaecat. Et dolore consequat tempor occaecat eiusmod cupidatat voluptate.
-      
-Commodo cillum officia duis cillum mollit sunt excepteur velit laboris reprehenderit id Lorem veniam consectetur. Laboris eiusmod aliquip dolore ex voluptate minim occaecat dolor est ut mollit dolore. Qui eiusmod anim duis cupidatat nostrud minim nostrud tempor amet est velit culpa non.
-      
-Laborum deserunt sit laborum Lorem sit in dolore velit aliquip. Non ex pariatur excepteur nisi ipsum reprehenderit amet laborum incididunt ad. Adipisicing tempor eiusmod enim fugiat sit velit ad quis ex reprehenderit deserunt.`);
+      setFileContent(`Aliqua reprehenderit minim ut amet ullamco magna deserunt do consectetur. Occaecat irure veniam duis laborum cupidatat ut ipsum aute. Labore deserunt laborum non et excepteur labore quis sint. Consectetur reprehenderit voluptate consequat ea non commodo proident quis voluptate fugiat non irure cillum.\n\nAute quis voluptate consequat tempor aliqua. Ipsum occaecat aliquip duis consequat elit quis consectetur duis minim eiusmod duis labore voluptate laborum. Elit in sint qui cillum ipsum Lorem dolor qui. Do id do culpa reprehenderit qui. In exercitation amet sunt ad ex sit commodo tempor. Culpa veniam eu id culpa labore qui cupidatat mollit deserunt.\n\nIncididunt sint voluptate officia magna laboris anim exercitation occaecat et nisi id. Eiusmod Lorem nostrud sint cupidatat aliquip. Do aute aliqua exercitation dolor culpa excepteur ut. Exercitation duis veniam officia id culpa irure tempor. Dolor fugiat mollit anim ex mollit irure. Veniam do amet et ex incididunt eiusmod incididunt qui eiusmod officia sunt voluptate ipsum cupidatat.\n\nAd aliqua eiusmod duis eu elit. Officia exercitation reprehenderit reprehenderit Lorem laboris id reprehenderit ex anim veniam dolore aute anim. Laborum eu do aute ea.\n\nVelit sunt consectetur velit aliquip Lorem. Laborum exercitation adipisicing in cupidatat. Nostrud culpa et pariatur fugiat commodo ad. Irure aliquip ut ullamco ad minim nisi.\n\nReprehenderit pariatur esse dolore pariatur ex fugiat nisi esse nulla deserunt dolor. Commodo incididunt consequat velit enim. Velit elit magna et consequat magna cillum cupidatat. Cupidatat deserunt pariatur nostrud ipsum consectetur incididunt deserunt nisi pariatur. Irure veniam Lorem eiusmod et excepteur ad dolore dolore. Incididunt elit cupidatat quis est exercitation minim.\n\nCupidatat cillum ea in enim occaecat et laborum officia nostrud duis ut deserunt culpa et. Quis ea amet et id ipsum veniam reprehenderit consectetur id duis esse. Esse laborum laboris sit Lorem sit voluptate in in laborum ipsum Lorem. Eiusmod commodo nulla eu cillum laboris laboris mollit voluptate nulla. Enim voluptate aliquip sit excepteur est. Elit nulla aliqua mollit anim exercitation labore cupidatat officia fugiat elit laborum do.\n\nMinim velit reprehenderit occaecat anim proident sunt in esse. Anim deserunt labore aliquip in amet labore ea sunt deserunt occaecat. Et dolore consequat tempor occaecat eiusmod cupidatat voluptate.\n\nCommodo cillum officia duis cillum mollit sunt excepteur velit laboris reprehenderit id Lorem veniam consectetur. Laboris eiusmod aliquip dolore ex voluptate minim occaecat dolor est ut mollit dolore. Qui eiusmod anim duis cupidatat nostrud minim nostrud tempor amet est velit culpa non.\n\nLaborum deserunt sit laborum Lorem sit in dolore velit aliquip. Non ex pariatur excepteur nisi ipsum reprehenderit amet laborum incididunt ad. Adipisicing tempor eiusmod enim fugiat sit velit ad quis ex reprehenderit deserunt.`.repeat(10));
     }, 1000);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
